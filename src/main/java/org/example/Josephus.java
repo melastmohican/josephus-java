15f4608db8404d1427462400b69c0e2c6e4cde8a
Mariusz S. Jurgielewicz <mordor@mail.com>
2017-10-15 11:13:57 -0700

    Adding Josephus class and test

---
+++ src/main/java/org/example/Josephus.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Created by mordor on 10/15/17.
+ */
+public class Josephus {
+    int josephus(int m, int n) {
+        return 0;
+    }
+}
---
+++ src/test/java/org/example/JosephusTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mordor on 10/15/17.
+ */
+public class JosephusTest {
+    @Test
+    public void josephus() throws Exception {
+    }
+
+}
