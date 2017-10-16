fb0f15c38396dddbaabbf3d254f35012a7f2e07b
Mariusz S. Jurgielewicz <mordor@mail.com>
2017-10-16 08:22:12 -0700

    Working on unit test

--- src/main/java/org/example/Josephus.java
+++ src/main/java/org/example/Josephus.java
@@ -1,10 +1,15 @@
 package org.example;
 
 /**
+ * g(n,k)=(g(n-1,k)+k){\bmod  n},{\text{ with }}g(1,k)=0
  * Created by mordor on 10/15/17.
  */
 public class Josephus {
-    int josephus(int m, int n) {
-        return 0;
+    public static int josephus(int n, int k) {
+        //if (n < 1) throw new IllegalArgumentException("n is out of range!");
+        //if (k < 1 || k >= n) throw new IllegalArgumentException("k is out of range!");
+        if (n == 1) return 0;
+
+        return (josephus(n-1, k) + k) % n;
     }
 }
--- src/test/java/org/example/JosephusTest.java
+++ src/test/java/org/example/JosephusTest.java
@@ -1,15 +1,46 @@
 package org.example;
 
 import org.junit.Test;
+import static org.example.Josephus.josephus;
 
 import static org.junit.Assert.*;
 
 /**
+ * Make sure your tests cover the following scenarios
+ * Invalid parameters
+ * What happens if n = k?
+ * What happens if n = 3 and k = 2?
+ * What happens if n is very large, but k =2?
+ * Choose a couple of other cases to test
+
  * Created by mordor on 10/15/17.
  */
 public class JosephusTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongN() throws Exception {
+        josephus(0,0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongK() throws Exception {
+        josephus(10,0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testKGreaterThanN() throws Exception {
+        josephus(2,3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testKEqualN() throws Exception {
+        int result = josephus(4,4);
+    }
+
     @Test
-    public void josephus() throws Exception {
+    public void testN3K2() throws Exception {
+        int result = josephus(3,2);
+        System.out.println(result);
     }
 
 }
